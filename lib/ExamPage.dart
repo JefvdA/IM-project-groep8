1076e3f9059ebf1dbbd50c8b693aa35f3880c393
tonyM <s123318@student.ap.be>
2022-04-28 14:26:35 +0200

    TA

--- lib/ExamPage.dart
+++ lib/ExamPage.dart
@@ -1,5 +1,5 @@
 // ignore_for_file: file_names
-
+import 'dart:async';
 import 'package:examap/test.dart';
 import 'package:flutter/material.dart';
 
@@ -17,6 +17,46 @@ class _ExamPageState extends State<ExamPage> {
     "Wat is de mooiste kleur ?",
     "Leg uit waarom dit de mooiste kleur is ?"
   ];
+  //timer
+  static const countdownDuration = Duration(hours: 3);
+  Duration _duration = Duration();
+  Timer? timer;
+
+  bool isCountdown = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    startTimer();
+    reset();
+  }
+
+  void reset() {
+    if (isCountdown) {
+      setState(() => _duration = countdownDuration);
+    } else {
+      setState(() => _duration = Duration());
+    }
+  }
+
+  void addTime() {
+    final addSeconds = isCountdown ? -1 : 1;
+
+    setState(() {
+      final seconds = _duration.inSeconds + addSeconds;
+      if (seconds < 0) {
+        timer?.cancel();
+      } else {
+        _duration = Duration(seconds: seconds);
+      }
+    });
+  }
+
+  void startTimer() {
+    timer = Timer.periodic(Duration(seconds: 1), (_) => addTime());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,10 +68,50 @@ class _ExamPageState extends State<ExamPage> {
             for (var i in question)
               Column(
                 children: [Text(i)],
-              )
+              ),
+            buildTime()
           ],
         ),
       ),
     );
   }
+
+  Widget buildTime() {
+    String twoDigits(int n) => n.toString().padLeft(2, '0');
+    final hours = twoDigits(_duration.inHours);
+    final minutes = twoDigits(_duration.inMinutes.remainder(60));
+    final seconds = twoDigits(_duration.inSeconds.remainder(60));
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        buildTimeCard(time: hours),
+        const SizedBox(width: 5),
+        buildTimeCard(time: minutes),
+        const SizedBox(width: 5),
+        buildTimeCard(time: seconds),
+      ],
+    );
+  }
+
+  Widget buildTimeCard({required String time}) => Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Container(
+            padding: EdgeInsets.all(2),
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Text(
+              time,
+              style: TextStyle(
+                fontWeight: FontWeight.normal,
+                color: Colors.white,
+                fontSize: 30,
+              ),
+            ),
+          ),
+          const SizedBox(height: 24),
+        ],
+      );
 }
