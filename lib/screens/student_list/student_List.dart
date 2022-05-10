7117b205222daaaa8a68ce4bd70a718c1df0cedc
JefvdA <jef.v.d.a@live.be>
2022-05-10 16:17:04 +0200

    Updated student_list.dart

--- lib/screens/student_list/student_List.dart
+++ lib/screens/student_list/student_List.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: file_names
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:examap/widgets/global_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class ListStudent extends StatelessWidget {
@@ -13,64 +14,85 @@ class ListStudent extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
-      home: ListOfStudent(),
+      home: const ListOfStudent(),
     );
   }
 }
 
-var _selectedValue;
 var setDefaultValue = true;
-var testing = "";
 
-class ListOfStudent extends StatelessWidget {
+class ListOfStudent extends StatefulWidget {
+  const ListOfStudent({Key? key}) : super(key: key);
+
+  @override
+  State<ListOfStudent> createState() => _ListOfStudentState();
+}
+
+class _ListOfStudentState extends State<ListOfStudent> {
+  final CollectionReference studentsCollection =
+      FirebaseFirestore.instance.collection('students');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('ExamAP'),
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            StreamBuilder<QuerySnapshot>(
-                stream: FirebaseFirestore.instance
-                    .collection('students')
-                    .snapshots(),
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    if (setDefaultValue) {
-                      _selectedValue = snapshot.data!.docs[0].get("s-nummer");
-                    }
-
-                    return ListView(
-                      shrinkWrap: true,
-                      children:
-                          snapshot.data!.docs.map((DocumentSnapshot document) {
-                        Map<String, dynamic> data =
-                            document.data()! as Map<String, dynamic>;
-                        return Card(
-                          color: Colors.white,
-                          child: ListTile(
-                            trailing: const Icon(Icons.keyboard_arrow_right),
-                            contentPadding: const EdgeInsets.all(8),
-                            title: Center(
-                                child: Text(
-                              data['s-nummer'].toString(),
-                              style: const TextStyle(
-                                fontWeight: FontWeight.normal,
-                                fontFamily: 'Roboto',
-                                color: Colors.red,
+      appBar: globalAppBar,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          StreamBuilder<QuerySnapshot>(
+            stream: studentsCollection.snapshots(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return ListView(
+                  shrinkWrap: true,
+                  children:
+                      snapshot.data!.docs.map((DocumentSnapshot document) {
+                    Map<String, dynamic> data =
+                        document.data()! as Map<String, dynamic>;
+                    return Card(
+                      color: Colors.white,
+                      child: ListTile(
+                        trailing: ElevatedButton.icon(
+                          icon: const Icon(Icons.delete),
+                          onPressed: () {},
+                          label: const Text(""),
+                        ),
+                        contentPadding: const EdgeInsets.all(8),
+                        title: Center(
+                          child: Column(
+                            children: [
+                              Text(
+                                data['s-nummer'].toString(),
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.normal,
+                                  fontFamily: 'Roboto',
+                                  color: Colors.black,
+                                ),
                               ),
-                            )),
+                              Text(
+                                data['name'].toString(),
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.normal,
+                                  fontFamily: 'Roboto',
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ],
                           ),
-                        );
-                      }).toList(),
+                        ),
+                      ),
                     );
-                  } else {
-                    return Container();
-                  }
-                }),
-          ],
-        ));
+                  }).toList(),
+                );
+              } else {
+                return Container();
+              }
+            },
+          ),
+        ],
+      ),
+    );
   }
+
+  void removeStudent() {}
 }
