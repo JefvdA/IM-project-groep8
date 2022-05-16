0be0f023925214a207865e402d6c53eb7f2963f6
jefvda <jef.v.d.a@live.be>
2022-05-16 11:25:02 +0200

    Edit questions small update

--- lib/screens/edit_questions/edit_questions_tab.dart
+++ lib/screens/edit_questions/edit_questions_tab.dart
@@ -27,30 +27,20 @@ class _EditQuestionsScreenState extends State<EditQuestionsScreen> {
     return Scaffold(
       appBar: globalAppBar,
       body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            if(widget.question['type'] == 'OQ')
-              EditOpenQuestionForm(widget.examId, widget.questionId, widget.question)
-            else if(widget.question['type'] == 'MC')
-              EditMultipleChoiceForm(widget.examId, widget.questionId, widget.question)
-            else if(widget.question['type'] == 'CC')
-              EditCodeCorrectionForm(widget.examId, widget.questionId, widget.question)
-          ],
-        ),
+        child: _buildEditQuestionForm(context)
       ),
     );
   }
 
   Widget _buildEditQuestionForm(BuildContext context) {
-    // if (_selectedValue == "OQ") {
-    //   return OpenQuestionForm(widget.examId, widget.questionId);
-    // } else if (_selectedValue == "MC") {
-    //   return MultipleChoiceForm(widget.examId, widget.questionId);
-    // } else if (_selectedValue == "CC") {
-    //   return CodeCorrectionForm(widget.examId, widget.questionId);
-    // }
-    return Container();
+    if(widget.question['type'] == 'OQ') {
+      return EditOpenQuestionForm(widget.examId, widget.questionId, widget.question);
+    } else if(widget.question['type'] == 'MC') {
+      return EditMultipleChoiceForm(widget.examId, widget.questionId, widget.question);
+    } else if(widget.question['type'] == 'CC') {
+      return EditCodeCorrectionForm(widget.examId, widget.questionId, widget.question);
+    } else {
+      return Container();
+    }
   }
 }
